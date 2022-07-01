17be3497c349ca733529a2dd0eece8780dae5d70
kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-01 22:52:15 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,28 +1,33 @@
-
 #include <stdio.h>
 
 /**
- * main - print triple combos
- *
- * Description: print triple digit combos
- *
+ * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i, j, k;
+	int n, m, l;
 
-	i = 48;
-	j = 48;
-	k = 48;
-
-	while (i < 58)
+	for (n = 48; n < 58; n++)
 	{
-		j = i + 1;
-		while (j < 58)
+		for (m = 49; m < 58; m++)
 		{
-			k = j + 1;
-			while (k < 58)
+			for (l = 50; l < 58; l++)
 			{
-		
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
