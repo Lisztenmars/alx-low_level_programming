602f4e8bd70706e7338f1fc1235e6cd7db1f1aad
kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-03 18:37:53 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 
 	if (n > 5)
 	{
-		printf("%d Last digit of 98 is 8 and is greater than 5\n", n);
+		printf("Last digit of 98 is 8 and is greater than 5\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d Last digit of 980 is 0 and is 0\n", n);
+		printf("Last digit of 980 is 0 and is 0\n", n);
 	}
 	else
 	{
-		printf("%d Last digit of -98 is -8 and is less than 6 and not 0\n", n);
+		printf("Last digit of -98 is -8 and is less than 6 and not 0\n", n);
 	}
 	return (0);
 }
