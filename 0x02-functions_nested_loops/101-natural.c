ab0485cbbcea03c925cc34e74bd04307c0986399
kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-05 22:41:20 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,22 @@
-ghkf
+#include <stdio.h>
+
+/**
+ * main - all natural numbers below 13
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
