bf575ff5420ab7ec1a79c86e4a98b86c140384ad
kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-12 11:28:22 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
-int _putchar(char c); 
+#ifndef _MAIN_H 
+ #define _MAIN_H 
+
  void reset_to_98(int *n); 
  void swap_int(int *a, int *b); 
+ int _putchar(char c); 
  int _strlen(char *s); 
  void _puts(char *str); 
  void print_rev(char *s); 
@@ -8,4 +11,6 @@ int _putchar(char c);
  void puts2(char *str); 
  void puts_half(char *str); 
  void print_array(int *a, int n); 
- char *_strcpy(char *dest, char *src);
+ char *_strcpy(char *dest, char *src); 
+ int _atoi(char *s); 
+ #endif
