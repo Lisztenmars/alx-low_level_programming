4b01615b95a9588b97ebb349381211d8afdb267f
kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-12 12:11:21 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,10 @@
   * @n: pointer number 
   */ 
 int main()
- void reset_to_98(int *n) 
- { 
+ { void reset_to_98(int *n) 
+ 
  *n = 98;/*reset the number to give us to 98*/ 
- }
+ Putchar("%p\n", n);
+
+  return 0;
+}
