8b0f2af834d7c0087757c70f1f754bffa98daf38
kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-13 15:39:39 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
- #ifndef FILE_HOLBERTON 
- #define FILE_HOLBERTON 
-  
  int _putchar(char c); 
  void reset_to_98(int *n); 
  void swap_int(int *a, int *b); 
@@ -14,4 +11,4 @@
  char *_strcpy(char *dest, char *src); 
  int _atoi(char *s); 
   
- #endif
+
