38921726358c718aa2320eec642f5d21cf4da256
Kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-21 06:25:56 -0700

    kings

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,12 +4,12 @@
  *int factorial(int n)  returns the factorial of a given number.
  * @n: number to return the factorial from
  *
- * Return:( -1) if n is lower than 0, return factorial otherwise.
+ * Return: 1 if n is lower than 0, return factorial otherwise.
  */
 int factorial(int n)
 {
 	if (n < 0)
-		return ( -1);
+		return (-1);
 	if (n == 0)
 		return (1);
 	return (n * factorial(n -1));
