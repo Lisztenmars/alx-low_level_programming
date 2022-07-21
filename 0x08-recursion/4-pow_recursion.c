1eb68b377f41d2518a2b69578ded1f73646e9323
Kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-21 07:55:54 -0700

    test

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,11 +7,11 @@
  *
  * Return: value of the exponentiation
  */
-int _power_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-       if (y == 0)
+	if (y == 0)
 		return (1);
-		return (x * _power_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
