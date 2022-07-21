87e8d3ae7932cb10785bee3a7ba7c81cbae53602
Kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-21 12:16:30 -0700

    test

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,7 @@
 #include "main.h"
 int actual_prime(int n, int i);
-
 /**
- * is_prime_number - says if an interger is is aprime number or not 
- * @n: number to evaluate
+ * is_prime_number - says if an interger  is a prime number or not * @n: number to evaluate
  *
  * Return: 1 if n is a prime number, 0 if not
  */
