cd3a8542fc3cce7b5a9506d95b20464e74664b79
Kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-23 15:58:41 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,33 +12,33 @@ int **alloc_grid(int width, int height)
 	int **tab, j, k;
 
 	tab = malloc(sizeof(*tab) * height);
-	
+
 	if (width <= 0 || height <= 0 || tab == 0)
 
 		return (NULL);
-	else
+	else if
 	{
 		for (j = 0; j < height; j++)
 		{
 			tab[j] = malloc(sizeof(**tab) * width);
-			
+
 			if (tab[j] == 0)
 			{
 				/*Free everything if malloc fails*/
 				while (j--)
-					
+
 					free(tab[j]);
-				
+
 				free(tab);
-				
+
 				return (NULL);
 			}
-			
+
 			for (k = 0; k < width; k++)
-				
+
 				tab[j][k] = 0;
 		}
-		
+
 		return (tab);
 	}
 }
