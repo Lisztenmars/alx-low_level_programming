b1fbd5d925a183f18ee3fb9bc9525610168572a2
Kingsley Ibrahim <kingsleynkau@gmail.com>
2022-07-23 13:25:29 -0700

    test

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,42 +3,42 @@
 
 /**
  * str_concat - concatinates two strings
- * @s1: string 1
- * @s2: string 2
+ * s1: string
+ * s2: string
  *
- * Return: pointer to concatinated strings
+ * Return: pointer to a string
  */
-char *str_concat(char s1, char s2)
+char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	int len1 = 0, int len2 = 0, j = 0, i = 0;
+	char a;
+	int i, j, c, d;
 
 	if (s1 == NULL)
 		s1 = " ";
+
 	if (s2 == NULL)
 		s2 = " ";
-	while (*(s1 + i))
-		len1++, i++;
-	while (*(s + j))
-		len2++, j++;
-	len2++;
 
-	concat = malloc(sizeof(char) * (len1 + len2));
-	
-	if (concat == NULL)
-		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++);
 
-	i = 0 j = 0;
-	while ( i < len1)
-	{
-		*(concat + i) = *(s1 + i);
-		i++;
-	}
-		while (j < len2)
-		{
-			if *(concat + i) = *(s2 + j);
-			i++, j++;
-		}
-		return (concat);
-}
-			
+	for (j = 0; s2[j] != '\0'; j++);
+
+	a = malloc((i * sizeof(*s1)) + ((j * sizeof(*s2)) + 1);
+
+			if (a == NULL)
+
+			return (NULL);
+
+			for (c = 0, d = 0; c < (i + j + 1); c++)
+			{
+			if (c < i)
+
+			a[c] = s1[c];
+
+			else
+
+			a[c] = s[d++];
+			}
+
+			return (a);
+			}
